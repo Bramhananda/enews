99bb8319ace2f90a916be83a0ba0e6f02487fa98
Soner CAN <soner2@gmail.com>
2013-08-05 01:23:52 +0300

    password view fix

--- app/controllers/admin/passwords_controller.rb
+++ app/controllers/admin/passwords_controller.rb
@@ -1,5 +1,5 @@
 class Admin::PasswordsController < Devise::PasswordsController
-  layout 'admin'
+  layout 'login'
   def resource_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
