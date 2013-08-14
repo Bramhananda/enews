567fc83a182a63c15a2ea83910f7f3195790243f
Soner CAN <soner2@gmail.com>
2013-08-15 00:20:00 +0300

    dfd

---
+++ analytics.rb
@@ -0,0 +1,46 @@
+# Inspired by https://gist.github.com/3166610
+require 'google/api_client'
+require 'date'
+
+# Update these to match your own apps credentials
+service_account_email = '805751889761@developer.gserviceaccount.com' # Email of service account
+key_file = '5f0d593e8923547709ad59cb155b1bc66d02edbd-privatekey.p12' # File containing your private key
+key_secret = 'notasecret' # Password to unlock private key
+profileID = '10267619' # Analytics profile ID.
+
+
+client = Google::APIClient.new()
+
+# Load our credentials for the service account
+key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
+client.authorization = Signet::OAuth2::Client.new(
+  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+  :audience => 'https://accounts.google.com/o/oauth2/token',
+  :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+  :issuer => service_account_email,
+  :signing_key => key)
+
+# Request a token for our service account
+client.authorization.fetch_access_token!
+
+analytics = client.discovered_api('analytics','v3')
+
+startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
+endDate = DateTime.now.strftime("%Y-%m-%d")
+
+visitCount = client.execute(:api_method => analytics.data.ga.get, :parameters => {
+  'ids' => "ga:" + profileID,
+  'start-date' => startDate,
+  'end-date' => endDate,
+  'dimensions' => "ga:day,ga:month",
+  'metrics' => "ga:visits",
+  'sort' => "ga:month,ga:day"
+})
+
+print visitCount.data.column_headers.map { |c|
+  c.name
+}.join("\t")
+
+visitCount.data.rows.each do |r|
+  print r.join("\t"), "\n"
+end
